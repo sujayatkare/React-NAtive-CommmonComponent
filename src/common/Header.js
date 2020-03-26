73d73b437e05cbd3da0d14de12bfe77c3db157bb
sujayatkare <53252801+sujayatkare@users.noreply.github.com>
2020-03-26 10:48:29 +0530

    Create Header.js

---
+++ src/common/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Container, Header, Left, Body, Right, Button, Title} from 'native-base';
+import {Image} from 'react-native';
+
+const HeaderScreen = ({navigation, title, isHeader}) => {
+  return (
+    <Container style={{marginTop:-20}}>
+      <Header>
+        <Left>
+          {isHeader ? (
+            <Button transparent onPress={() => navigation.goBack()}>
+              <Image
+                source={require('../../assets/icons/button.jpg')}
+                style={{width: 20, height: 20}}
+              />
+            </Button>
+          ) : null}
+        </Left>
+        <Body>
+          <Title>{title}</Title>
+        </Body>
+        <Right />
+      </Header>
+    </Container>
+  );
+};
+
+export default HeaderScreen;
