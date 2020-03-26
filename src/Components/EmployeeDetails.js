3565a5bfebcb79d2cb92b522a9f2aea909826c48
sujayatkare <53252801+sujayatkare@users.noreply.github.com>
2020-03-26 10:50:42 +0530

    Create EmployeeDetails.js

---
+++ src/Components/EmployeeDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const EmployeeDetails = () => {
+  return (
+    <View>
+      <Text>This is Employee Details</Text>
+    </View>
+  );
+};
+
+export default EmployeeDetails;
